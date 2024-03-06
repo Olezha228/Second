b63c56b2b16b756cea30057c317d01eeeb6a0cc5
Олег <@DESKTOP-D64AONM>
2024-03-06 18:47:40 +0500

    Add project files.

---
+++ Second/Controllers/RemoteDataController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Second.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RemoteDataController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public RemoteDataController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var response = await _httpClient.GetAsync("http://host.docker.internal:32768/Data");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                return Ok(data);
+            }
+
+            return StatusCode((int)response.StatusCode, "Error fetching data from ProjectA");
+        }
+    }
+}
---
+++ SecondTests/RemoteDataControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using Second.Controllers;
+
+namespace YourNamespace.Tests
+{
+    public class RemoteDataControllerTests
+    {
+        private Mock<HttpClient> _mockHttpClient;
+        private RemoteDataController _controller;
+
+        public RemoteDataControllerTests()
+        {
+            _mockHttpClient = new Mock<HttpClient>();
+            _controller = new RemoteDataController(_mockHttpClient.Object);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsSuccessResult_WhenDataIsRetrievedSuccessfully()
+        {
+            // Arrange
+            var expectedData = "{\"key\":\"value\"}";
+            _mockHttpClient.Setup(httpClient => httpClient.GetAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(expectedData)
+                }));
+
+            // Act
+            var result = await _controller.Get();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var actualData = JsonConvert.DeserializeObject<string>(okResult.Value.ToString());
+            Assert.Equal(expectedData, actualData);
+        }
+
+        [Fact]
+        public async Task Get_ReturnsErrorResult_WhenDataCannotBeRetrieved()
+        {
+            // Arrange
+            _mockHttpClient.Setup(httpClient => httpClient.GetAsync(It.IsAny<string>()))
+                .Returns(Task.FromResult(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.InternalServerError
+                }));
+
+            // Act
+            var result = await _controller.Get();
+
+            // Assert
+            var statusCodeResult = Assert.IsType<StatusCodeResult>(result);
+            Assert.Equal((int)HttpStatusCode.InternalServerError, statusCodeResult.StatusCode);
+        }
+    }
+}
